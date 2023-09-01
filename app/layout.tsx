99f1c690c5ae2425e77895c44afbd67f1dbff42a
Ross Ragsdale <ross@pointblankdev.com>
2023-09-01 12:54:06 -0500

    :chart_with_upwards_trend: Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(roboto.className, 'container', 'my-8')}>{children}</body>
+      <Analytics />
     </html>
   )
 }
