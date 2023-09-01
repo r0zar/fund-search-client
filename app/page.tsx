54ba80451bd44a91005a0fb7edad5669e2e5f97e
Ross Ragsdale <ross@pointblankdev.com>
2023-09-01 04:35:47 -0500

    :truck: Move search to base route

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,158 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import algoliasearch from "algoliasearch/lite";
+import { Hit as AlgoliaHit } from "instantsearch.js";
+import React, { useCallback } from "react";
+import {
+    InstantSearch,
+    Highlight,
+    InfiniteHits,
+    UseSearchBoxProps,
+} from "react-instantsearch";
+import "./page.css";
+import { MdSearch, MdClose } from "react-icons/md";
+import { useSearchBox } from 'react-instantsearch';
+
+const searchClient = algoliasearch(
+    "GLZSNNWVWG",
+    "4738f41bcd261b41738d9605b99170c0"
+);
+
+type HitProps = {
+    hit: AlgoliaHit<{
+        ticker: string;
+        name: string;
+        exchange: string;
+    }>;
+};
+
+function Hit({ hit }: HitProps) {
+    return (
+        <article className="grid grid-cols-3 w-full p-2 border-b focus:ring-2" tabIndex={0}>
+            <Highlight hit={hit} attribute="name" highlightedTagName="b" />
+            <Highlight hit={hit} attribute="ticker" highlightedTagName="b" />
+            <span>{hit.exchange}</span>
+        </article>
+    );
+}
+
+
+function CustomSearchBox(props: UseSearchBoxProps | any) {
+    const { query, refine } = useSearchBox(props);
+    const [inputValue, setInputValue] = React.useState(query);
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    function setQuery(newQuery: string) {
+        setInputValue(newQuery);
+
+        refine(newQuery);
+    }
+
+    return (
+        <div>
+            <form
+                action=""
+                role="search"
+                noValidate
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    if (inputRef.current) {
+                        inputRef.current.blur();
+                    }
+                }}
+                onReset={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    setQuery('');
+
+                    if (inputRef.current) {
+                        inputRef.current.focus();
+                    }
+                }}
+                className="flex justify-center"
+            >
+                <div className="flex bg-white w-min relative rounded-lg">
+
+                    <MdSearch size={20} className="absolute left-2 top-2.5 submit-icon" />
+                    <input
+                        className="px-8 py-2 w-80 rounded-lg focus:outline-none bg-white"
+                        ref={inputRef}
+                        autoComplete="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        placeholder="Search for securities"
+                        spellCheck={false}
+                        maxLength={512}
+                        value={inputValue}
+                        onChange={(event) => {
+                            setQuery(event.currentTarget.value);
+                        }}
+                        autoFocus
+                        onKeyDown={(e: any) => {  // This allows it to be "clickable" using Enter or Space
+                            if (e.key === 'Escape') {
+                                props.setModalOpen(false);
+                            }
+                        }}
+                    />
+                    <MdClose
+                        className='absolute right-2 top-2.5 reset-icon focus:outline-none'
+                        size={20}
+                        tabIndex={0}
+                        onClick={() => {
+                            props.setModalOpen(false)
+                            setQuery('')
+                        }}
+                        onKeyDown={(e: any) => {  // This allows it to be "clickable" using Enter or Space
+                            if (e.key === 'Enter' || e.key === ' ') {
+                                props.setModalOpen(false);
+                                setQuery('');
+                            }
+                        }}
+                    />
+                </div>
+            </form>
         </div>
-      </div>
+    );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function App() {
+
+    const [modalOpen, setModalOpen] = React.useState(false)
+
+    const queryHook = useCallback((query: any, search: (arg: any) => void) => {
+        if (query.length >= 2) {
+            search(query);
+            setModalOpen(true)
+        } else {
+            setModalOpen(false)
+        }
+    }, []);
+
+    return (
+        <InstantSearch
+            searchClient={searchClient}
+            indexName="funds"
+            routing={true}
+            insights={true}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <CustomSearchBox
+                queryHook={queryHook}
+                setModalOpen={setModalOpen}
+            />
+            {modalOpen && (
+                <div className="rounded-lg shadow-modal mt-4 p-4 border bg-white  max-h-[80vh] overflow-y-scroll">
+                    <thead className="grid grid-cols-3 table-header p-2 text-secondary">
+                        <div className="">Name</div><div>Ticker</div><div>Exchange</div>
+                    </thead>
+                    <InfiniteHits showPrevious={false} hitComponent={Hit} className="w-full border-0" />
+                </div>
+            )}
+            <div className="fixed inset-0 -z-10" onClick={() => setModalOpen(false)}></div>
+        </InstantSearch>
+    );
 }
